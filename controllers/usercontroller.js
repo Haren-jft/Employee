cd3dcffc31f8ceadc76a77dee717cdbb31c6b3dc
Haren-jft <haren.sharma@jellyfishtechnologies.com>
2022-12-11 19:15:12 +0530

    express

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,28 +1,19 @@
 const User=require('../models/usermodel');
 //@desc get all the users
-//@route /api/users
 async function getUsers(req,res){
     try {
         const users=await User.findAll();
-        res.writeHead(200,{'Content-Type':'application/json'});
-        res.end(JSON.stringify(users));
-    } catch (error) {
-        console.log(error);
-    }
+        res.json(users);
+    } catch (error) {console.log(error);}
 }
-//@desc      get single user
-//@route     GET /api/users/:id
+//@desc get single user
 async function getUser(req,res,id){
     try {
         const user=await User.findById(id);
-        res.writeHead(200,{'Content-Type':'application/json'});
-        res.end(JSON.stringify(user));
-    } catch (error) {
-        console.log(error);
-    }
+        res.json(user);
+    } catch (error) {console.log(error);}
 }
 //@desc      create user
-//@route     Post /api/users
 async function createUser(req,res){
     try {
         let body='';
@@ -37,28 +28,16 @@ async function createUser(req,res){
                 salary 
             }
             const newUser=await User.create(user);
-            console.log(newUser);
-            res.writeHead(201,{'Content-Type':'application/json'})
-            res.end(JSON.stringify(newUser));
+            res.json(newUser);
         })
-    } catch (error) {
-        console.log(error);
-    }
+    } catch (error) {console.log(error);}
 }
 //@desc      update user
-//@route     Put /api/users
 async function updateUser(req,res,id){
     const user=await User.findById(id);
-    if(!user){
-        res.writeHead(404,{'Content-Type':'application/json'})
-        res.end(JSON.stringify({message:'User Not Found'}))
-    }
-    else{
         try {
             let body='';
-            req.on('data',(chunk)=>{
-                body+=chunk;
-            })
+            req.on('data',(chunk)=>body+=chunk)
             req.on('end',async()=>{
                 const {name,job,salary}=JSON.parse(body);
                 const user_data={
@@ -67,25 +46,15 @@ async function updateUser(req,res,id){
                     salary:salary || user.salary 
                 }
                 const updUser=await User.update(id,user_data);
-                res.writeHead(200,{'Content-Type':'application/json'})
-                res.end(JSON.stringify(updUser));
+                res.json(updUser);
             })
-        } catch (error) {
-            console.log(error);
-        }    
+        } catch (error) {console.log(error);}    
     }
-}
 async function deleteUser(req,res,id){
     try {
-        console.log('demo')
-        console.log(id);
         await User.remove(id);
-        console.log('demo')
-        res.writeHead(200,{'Content-Type':'application/json'});
-        res.end(JSON.stringify({message:`User ${id} removed`}));
-    } catch (error) {
-        console.log(error);
-    }
+        res.json({message:`User ${id} removed`})
+    } catch (error) {console.log(error);}
 }
 module.exports={
     getUsers,
--- index.js
+++ index.js
@@ -1,36 +1,13 @@
-const http=require('http');
-const { getUsers,getUser,createUser,updateUser,deleteUser }=require('./controllers/usercontroller');
+const express = require('express')
+const cors = require('cors');
+const { getUsers, getUser,createUser,updateUser, deleteUser } = require('./controllers/usercontroller');
+const app = express()
+app.use(cors())
 const PORT=process.env.PORT || 3000;
-const server=http.createServer((req,res)=>{
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    // res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, content-type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    if(req.method==='OPTIONS'){
-        res.writeHead(200);
-        res.end();
-    }
-    else if(req.url==='/users' && req.method==='GET'){
-        getUsers(req,res);
-    }
-    else if(req.url.match(/\/users\/([0-9]+)/) && req.method==='GET'){
-        const id=req.url.split('/')[2];
-        getUser(req,res,id);
-    }
-    else if(req.url==='/users' && req.method==='POST'){
-        createUser(req,res);
-    }
-    else if(req.url.match(/\/users\/([0-9]+)/) && req.method==='PUT'){
-        const id=req.url.split('/')[2];
-        updateUser(req,res,id);
-    }
-    else if(req.url.match(/\/users\/([0-9]+)/) && req.method==='DELETE'){
-        const id=req.url.split('/')[2];
-        deleteUser(req,res,id);
-    }
-    else{   
-        res.writeHead(404,{'Content-Type':'text/plain'});
-        res.end("404 Page Not Found");
-    }
-});
-server.listen(PORT,()=>{console.log(`Server is running at ${PORT}`)});
+app.get('/users',(req,res)=>getUsers(req,res));
+app.get('/users/:id',(req,res)=>getUser(req,res,parseInt(req.params.id)));
+app.post('/users',(req,res)=>createUser(req,res));
+app.put('/users/:id',(req,res)=>updateUser(req,res,parseInt(req.params.id)));
+app.delete('/users/:id',(req,res)=>deleteUser(req,res,parseInt(req.params.id)));
+app.listen(PORT,()=>{console.log(`Server is running at ${PORT}`)});
+
